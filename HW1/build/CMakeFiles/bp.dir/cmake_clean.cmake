1462e872a413b896a2b8c8fbfbaf25f1360d3d77
Shai Yehezkel <shai444@gmail.com>
2021-04-24 14:33:25 +0300

    First commit

---
+++ HW1/build/CMakeFiles/bp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bp.dir/bp_main.c.o"
+  "bp.pdb"
+  "bp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
