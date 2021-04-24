9076aa9ce843080c6e55b02496dd493e37a148c7
Shai Yehezkel <shai444@gmail.com>
2021-04-24 16:50:43 +0300

    דג

--- HW1/bp.cpp
+++ HW1/bp.cpp
@@ -1,21 +1,23 @@
+/* 046267 Computer Architecture - Spring 2020 - HW #1 */
+/* This file should hold your implementation of the predictor simulator */
 
 #include "bp_api.h"
 
+
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
-bool isGlobalHist, bool isGlobalTable, int Shared)
-{
-
+			bool isGlobalHist, bool isGlobalTable, int Shared){
+	return -1;
 }
 
-bool BP_predict(uint32_t pc, uint32_t *dst)
-{
+bool BP_predict(uint32_t pc, uint32_t *dst){
+	return false;
+}
 
+void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
+	return;
 }
-void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst)
-{
 
+void BP_GetStats(SIM_stats *curStats){
+	return;
 }
-void BP_GetStats(SIM_stats *curStats)
-{
-    
-}
+
