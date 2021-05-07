2fed92bc18b75eeddf35395eae5a27252024ba31
Shai Yehezkel <shai444@gmail.com>
2021-05-08 00:29:32 +0300

    Added initial functions and data structures

---
+++ HW2/build/CMakeFiles/CacheSim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CacheSim.dir/cacheSim.cpp.o"
+  "CMakeFiles/CacheSim.dir/cacheSim.cpp.o.d"
+  "CacheSim"
+  "CacheSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CacheSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- HW2/cacheSim.cpp
+++ HW2/cacheSim.cpp
@@ -4,18 +4,79 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
-using std::FILE;
-using std::string;
+#include <vector>
+#include <list>
+#include <math.h>
+using std::cerr;
 using std::cout;
 using std::endl;
-using std::cerr;
+using std::FILE;
 using std::ifstream;
+using std::string;
 using std::stringstream;
+using std::vector;
 
-int main(int argc, char **argv) {
+class CacheBlock_p
+{
+	time_t last_accessed_time;
+	// TODO c'tor & d'tor !
+	//vector<CacheBlock>::iterator pos_in_vector;
+};
+typedef struct CacheBlock_p *CacheBlock;
+class Way
+{
+	CacheBlock *blocks;
 
-	if (argc < 19) {
+	// TODO think of a data structure for this
+	//std::vector<CacheBlock> least_recently_used_blocks; // Sorted such that the least used is always at the back
+
+	// TODO c'tor & d'tor !
+};
+
+class CacheLevel
+{
+	// num_of_blocks = cache_size/block_size
+	// num_of_sets = num_of_blocks/num_of_ways
+	// tag_size = 32 - (log2(num_of_sets)) - (log2(block_size))
+	unsigned int cache_byte_size;
+	unsigned int association;
+	unsigned int num_of_cycles; // Mask for keeping history in correct size
+	unsigned int block_byte_size;
+
+public:
+};
+enum WritePolicy
+{
+	NO_WRITE_ALLOCATE = 0,
+	WRITE_ALLOCATE = 1
+};
+class Cache
+{
+
+	WritePolicy write_policy;
+	CacheLevel L1;
+	CacheLevel L2;
+
+public:
+	double L1MissRate;
+	double L2MissRate;
+	double avgAccTime;
+	Cache(const unsigned &MemCyc, const unsigned &BSize, const unsigned &L1Size, const unsigned &L2Size,
+		  const unsigned &L1Assoc, const unsigned &L2Assoc, const unsigned &L1Cyc,
+		  const unsigned &L2Cyc, const unsigned &WrAlloc)
+	{
+		// TODO initialzize cache levels
+	}
+	~Cache() = delete;
+
+	void cacheRead();
+	void cacheWrite();
+};
+int main(int argc, char **argv)
+{
+
+	if (argc < 19)
+	{
 		cerr << "Not enough arguments" << endl;
 		return 0;
 	}
@@ -24,50 +85,74 @@ int main(int argc, char **argv) {
 
 	// File
 	// Assuming it is the first argument
-	char* fileString = argv[1];
+	char *fileString = argv[1];
 	ifstream file(fileString); //input file stream
 	string line;
-	if (!file || !file.good()) {
+	if (!file || !file.good())
+	{
 		// File doesn't exist or some other error
 		cerr << "File not found" << endl;
 		return 0;
 	}
 
 	unsigned MemCyc = 0, BSize = 0, L1Size = 0, L2Size = 0, L1Assoc = 0,
-			L2Assoc = 0, L1Cyc = 0, L2Cyc = 0, WrAlloc = 0;
+			 L2Assoc = 0, L1Cyc = 0, L2Cyc = 0, WrAlloc = 0;
 
-	for (int i = 2; i < 19; i += 2) {
+	for (int i = 2; i < 19; i += 2)
+	{
 		string s(argv[i]);
-		if (s == "--mem-cyc") {
+		if (s == "--mem-cyc")
+		{
 			MemCyc = atoi(argv[i + 1]);
-		} else if (s == "--bsize") {
+		}
+		else if (s == "--bsize")
+		{
 			BSize = atoi(argv[i + 1]);
-		} else if (s == "--l1-size") {
+		}
+		else if (s == "--l1-size")
+		{
 			L1Size = atoi(argv[i + 1]);
-		} else if (s == "--l2-size") {
+		}
+		else if (s == "--l2-size")
+		{
 			L2Size = atoi(argv[i + 1]);
-		} else if (s == "--l1-cyc") {
+		}
+		else if (s == "--l1-cyc")
+		{
 			L1Cyc = atoi(argv[i + 1]);
-		} else if (s == "--l2-cyc") {
+		}
+		else if (s == "--l2-cyc")
+		{
 			L2Cyc = atoi(argv[i + 1]);
-		} else if (s == "--l1-assoc") {
+		}
+		else if (s == "--l1-assoc")
+		{
 			L1Assoc = atoi(argv[i + 1]);
-		} else if (s == "--l2-assoc") {
+		}
+		else if (s == "--l2-assoc")
+		{
 			L2Assoc = atoi(argv[i + 1]);
-		} else if (s == "--wr-alloc") {
+		}
+		else if (s == "--wr-alloc")
+		{
 			WrAlloc = atoi(argv[i + 1]);
-		} else {
+		}
+		else
+		{
 			cerr << "Error in arguments" << endl;
 			return 0;
 		}
 	}
 
-	while (getline(file, line)) {
+	Cache cache(MemCyc, BSize, L1Size, L2Size, L1Assoc, L2Assoc, L1Cyc, L2Cyc, WrAlloc);
+	while (getline(file, line))
+	{
 
 		stringstream ss(line);
 		string address;
 		char operation = 0; // read (R) or write (W)
-		if (!(ss >> operation >> address)) {
+		if (!(ss >> operation >> address))
+		{
 			// Operation appears in an Invalid format
 			cout << "Command Format error" << endl;
 			return 0;
@@ -87,15 +172,20 @@ int main(int argc, char **argv) {
 		// DEBUG - remove this line
 		cout << " (dec) " << num << endl;
 
+		switch (operation)
+		{
+		case 'R':
+			cache.cacheRead(); // Lior
+			break;
+		case 'W':
+			cache.cacheWrite(); // Shai
+			break;
+		}
 	}
 
-	double L1MissRate;
-	double L2MissRate;
-	double avgAccTime;
-
-	printf("L1miss=%.03f ", L1MissRate);
-	printf("L2miss=%.03f ", L2MissRate);
-	printf("AccTimeAvg=%.03f\n", avgAccTime);
+	printf("L1miss=%.03f ", cache.L1MissRate);
+	printf("L2miss=%.03f ", cache.L2MissRate);
+	printf("AccTimeAvg=%.03f\n", cache.avgAccTime);
 
 	return 0;
 }
