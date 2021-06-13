8dde29977caf49f57ce76c3a7b8933f3cb33e563
Shai Yehezkel <shai444@gmail.com>
2021-06-13 20:41:02 +0300

    Added structs

--- HW4/core_api.cpp
+++ HW4/core_api.cpp
@@ -5,12 +5,48 @@
 
 #include <stdio.h>
 
+typedef struct _thread
+{
+	unsigned int return_cycle;
+	tcontext regs;
+} Thread;
+
+typedef struct _core
+{
+	unsigned int num_of_threads;
+	unsigned int cycle_count;
+	unsigned int inst_count;
+	unsigned int rr_index;
+	Thread *core_threads;
+} Core;
+
+Core core_finegrained;
+
+void CORE_init(Core *core)
+{
+	core->cycle_count = 0;
+	core->inst_count = 0;
+	core->rr_index = 0;
+	core->num_of_threads = SIM_GetThreadsNum();
+	core->core_threads = new Thread[core->num_of_threads];
+
+	for (unsigned int i = 0; i < core->num_of_threads; i++)
+	{
+		core->core_threads[i].return_cycle = 0;
+		for (unsigned int j = 0; j < REGS_COUNT; j++)
+		{
+			core->core_threads[i].regs.reg[j] = 0;
+		}
+	}
+}
 void CORE_BlockedMT()
 {
 }
 
 void CORE_FinegrainedMT()
 {
+
+	CORE_init(&core_finegrained);
 }
 
 double CORE_BlockedMT_CPI()
@@ -20,6 +56,7 @@ double CORE_BlockedMT_CPI()
 
 double CORE_FinegrainedMT_CPI()
 {
+	delete[] core.core_threads;
 	return 0;
 }
 
